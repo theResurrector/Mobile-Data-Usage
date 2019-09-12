d86f350dcf965df8cbd444e5206bb131ca4074f4
Anson Ng <anson@codigo.co>
2019-09-12 10:28:48 +0800

    Chore: added project to folder

---
+++ MobileDataUsage/MobileDataUsage/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MobileDataUsage
+//
+//  Created by Anson on 12/9/19.
+//  Copyright © 2019 Anson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
