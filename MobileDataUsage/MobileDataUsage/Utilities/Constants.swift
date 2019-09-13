9bfd337f2d5cf05c20b52cb621fbd5003d8d435f
Anson Ng <anson@codigo.co>
2019-09-13 16:43:01 +0800

    chore: moved files to respective groups

---
+++ MobileDataUsage/MobileDataUsage/Utilities/Constants.swift
@@ -0,0 +1,14 @@
+//
+//  Constants.swift
+//  MobileDataUsage
+//
+//  Created by Anson on 12/9/19.
+//  Copyright © 2019 Anson. All rights reserved.
+//
+
+import Foundation
+
+struct Constants {
+    static let baseUrl = "https://data.gov.sg/"
+    
+}
